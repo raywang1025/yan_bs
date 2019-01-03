c2a8f97e6bf0e04a2f0e08bcdeeae77326fdbe4a
ray <ray@rayde-MacBook-Pro.local>
2019-01-03 22:49:11 +0800

    add animation and google map api

--- js/all.js
+++ js/all.js
@@ -1,7 +1,39 @@
-$(window).scroll(function(e){
-  if($(window).scrollTop()>0)
-    $('nav').removeClass('navbar-top')
-  else
-    $('nav').addClass('navbar-top')
-  var s = skrollr.init();
+$(document).ready(function(){
+  
+  $(window).scroll(function(e){
+    if($(window).scrollTop()>0)
+      $('nav').removeClass('navbar-top')
+    else
+      $('nav').addClass('navbar-top')
+    var s = skrollr.init();
+  })
+
+  $("#scrollToAbout").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $('#about').offset().top
+    }, 2000);
+  });
+
+  $("#scrollToPersist").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $('#persist').offset().top
+    }, 2000);
+  });
+
+  $("#scrollToProduct").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $('#product').offset().top
+    }, 2000);
+  });
+
+  $("#scrollToLocation").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $('#location').offset().top
+    }, 2000);
+  });    
+
 })
