39ac3701f91e9ede7bb3a2ede34e7aa2e7379cfc
Ray Wang <raydingwang@gmail.com>
2021-05-31 17:10:03 +0800

    update

--- js/all.js
+++ js/all.js
@@ -1,46 +1,39 @@
-$(document).ready(function(){
-  
-  $(window).scroll(function(e){
-    if($(window).scrollTop()>0)
-      $('nav').removeClass('navbar-top')
-    else
-      $('nav').addClass('navbar-top')
-    var s = skrollr.init();
-  })
+$(document).ready(function() {
 
-  $("#scrollToAbout").click(function(e) {
-    e.preventDefault();
-    $('html, body').animate({
-        scrollTop: $('#about').offset().top
-    }, 2000);
-  });
+    $(window).scroll(function(e) {
+        if ($(window).scrollTop() > 0)
+            $('nav').removeClass('navbar-top')
+        else
+            $('nav').addClass('navbar-top')
+        var s = skrollr.init();
+    })
 
-  $("#scrollToPersist").click(function(e) {
-    e.preventDefault();
-    $('html, body').animate({
-        scrollTop: $('#persist').offset().top
-    }, 2000);
-  });
-
-  $("#scrollToProduct").click(function(e) {
-    e.preventDefault();
-    $('html, body').animate({
-        scrollTop: $('#product').offset().top
-    }, 2000);
-  });
-
-  $("#scrollToLocation").click(function(e) {
-    e.preventDefault();
-    $('html, body').animate({
-        scrollTop: $('#location').offset().top
-    }, 2000);
-  });    
-
-  window.addEventListener("load", function(event) {
-    var seeLocation = document.querySelector('.where');
-    seeLocation.addEventListener('click',function(e){
-      e.preventDefault();
-      mixpanel.track("seeLocation");
+    $("#scrollToAbout").click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('#about').offset().top
+        }, 2000);
     });
- });   
+
+    $("#scrollToPersist").click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('#persist').offset().top
+        }, 2000);
+    });
+
+    $("#scrollToProduct").click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('#product').offset().top
+        }, 2000);
+    });
+
+    $("#scrollToLocation").click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('#location').offset().top
+        }, 2000);
+    });
+
 });
